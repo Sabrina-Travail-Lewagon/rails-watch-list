ea90bc54c4916890dbcd21fa33ff91ba77049b51
Sabrina MOUEDINE <mouedines@cfdt-services.fr>
2023-05-26 22:03:45 +0200

    ajout active storage cloudinary env

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,5 @@
 class List < ApplicationRecord
+  has_one_attached :photo
   has_many :bookmarks, dependent: :destroy
   # On lie la table à movie (liaison many to many)
   has_many :movies, through: :bookmarks
